655534965cfc6bab2371bb516c5b32583af45180
zappzarazupp <dev@zappzarazupp.de>
2019-01-21 19:18:22 +0100

    clean up test code (1)

--- src/main.cpp
+++ src/main.cpp
@@ -8,24 +8,8 @@
 #include <WiFiUdp.h>
 #include <WiFipwd.h>
 
+// The display
 SSD1306  display(0x3c, 21, 22);
-
-const uint16_t PixelCount = 7;
-const uint8_t PixelPin = 2;
-#define colorSaturation 255
-
-NeoPixelBus<NeoGrbwFeature, Neo800KbpsMethod> strip(PixelCount, PixelPin);
-
-RgbwColor red(colorSaturation, 0, 0, 0);
-RgbwColor green(0, colorSaturation, 0, 0);
-RgbwColor blue(0, 0, colorSaturation, 0);
-RgbwColor white1(colorSaturation, colorSaturation, colorSaturation, 0);
-RgbwColor white2(0,0,0,colorSaturation);
-RgbwColor white3(colorSaturation, colorSaturation, colorSaturation, colorSaturation);
-RgbwColor mult(0,colorSaturation,0,0);
-RgbwColor black(0);
-uint8_t gg=0;
-uint8_t ff=0;
 
 ////
 // in WiFipwd.h
@@ -33,55 +17,61 @@ uint8_t ff=0;
 //const char *password = "password";
 ////
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 3600, 60000);
-uint8_t oldt = 255;
+NTPClient timeClient(ntpUDP, "192.168.42.99", 3600, 60000); //todo: adjust timeoffset to DST automatically
 
-const uint8_t Touch1 = 36;
+// Char LEDs (Matrix)
+const uint8_t PanelWidth = 11;
+const uint8_t PanelHeight = 10;
+const uint16_t PanelPixelCount = PanelWidth * PanelHeight;
+const uint8_t PanelPixelPin = 2;
+NeoTopology <RowMajorLayout> topo(PanelWidth, PanelHeight);
+NeoPixelBus <NeoGrbwFeature, Neo800KbpsMethod> PanelStrip(PanelPixelCount, PanelPixelPin);
+
+// Other LEDs (minutes 1-4, 8 touch keys)
+const uint16_t OtherPixelCount = 12;
+const uint8_t OtherPixelPin = 3;
+NeoPixelBus <NeoGrbwFeature, Neo800KbpsMethod> OtherStrip(PanelPixelCount, PanelPixelPin);
+
+// globals
+uint8_t minBrightness = 10;
+uint8_t maxBrightness = 255;
+uint8_t oldt = 255; // store second value for refresh
 
 void setup() {
  
   display.init();
   display.flipScreenVertically();
   display.clear();
-  display.drawString(0, 0, "Hello World");
   display.display();
 
-  strip.Begin();
-  strip.Show();
+  PanelStrip.Begin();
+  PanelStrip.ClearTo(RgbwColor(0,0,0,255));
+  PanelStrip.Show();
+  OtherStrip.Begin();
+  OtherStrip.ClearTo(RgbwColor(0,0,0,255));
+  OtherStrip.Show();
 
   WiFi.begin(ssid, password);
 
   while ( WiFi.status() != WL_CONNECTED ) {
     display.clear();
-    delay ( 200 );
-    display.drawString(0, 0, "Connect Wifi");
+    display.display();
+    delay ( 300 );
+    display.drawString(0, 0, "Connecting\n to\n Wifi ...");
     display.display();
     delay ( 300 );
   }
 
-    display.clear();
-    display.drawString(0, 0, "Wifi Connected");
-    display.invertDisplay();
-    display.display();
-
+  display.clear();
+  display.drawString(0, 0, "Wifi Connected");
+  display.display();
 
   timeClient.begin();
-
-  pinMode(Touch1, INPUT);
-
 }
- 
-void loop() {
 
-  if(digitalRead(Touch1) == 1) {
-    strip.SetPixelColor(6, mult);
-    strip.Show();
-  }
-  else {
-    strip.SetPixelColor(6, black);
-    strip.Show();
-  }
-  
+uint8_t ff=0;
+
+void loop() {
 
   if(timeClient.getSeconds() != oldt) {
     timeClient.update();
@@ -92,23 +82,25 @@ void loop() {
     oldt = timeClient.getSeconds();
 
     if(ff == 0) {
-      strip.SetPixelColor(0, red);
-      strip.SetPixelColor(1, green);
-      strip.SetPixelColor(2, blue);
-      strip.SetPixelColor(3, white1);
-      strip.SetPixelColor(4, white2);
-      strip.SetPixelColor(5, white3);
-      strip.Show();
+      PanelStrip.SetPixelColor(0, RgbwColor(255,0,0,0));
+      PanelStrip.SetPixelColor(1, RgbwColor(0,255,0,0));
+      PanelStrip.SetPixelColor(2, RgbwColor(0,0,255,0));
+      PanelStrip.SetPixelColor(3, RgbwColor(255,0,0,255));
+      PanelStrip.SetPixelColor(4, RgbwColor(0,255,0,255));
+      PanelStrip.SetPixelColor(5, RgbwColor(0,0,255,255));
+      PanelStrip.SetPixelColor(6, RgbwColor(255,255,255,255));
+      PanelStrip.Show();
       ff=1;
     }
     else {
-      strip.SetPixelColor(0, black);
-      strip.SetPixelColor(1, black);
-      strip.SetPixelColor(2, black);
-      strip.SetPixelColor(3, black);
-      strip.SetPixelColor(4, black);
-      strip.SetPixelColor(5, black);
-      strip.Show();
+      PanelStrip.SetPixelColor(0, RgbwColor(0,0,0,0));
+      PanelStrip.SetPixelColor(1, RgbwColor(0,0,0,0));
+      PanelStrip.SetPixelColor(2, RgbwColor(0,0,0,0));
+      PanelStrip.SetPixelColor(3, RgbwColor(0,0,0,0));
+      PanelStrip.SetPixelColor(4, RgbwColor(0,0,0,0));
+      PanelStrip.SetPixelColor(5, RgbwColor(0,0,0,0));
+      PanelStrip.SetPixelColor(6, RgbwColor(0,0,0,0));
+      PanelStrip.Show();
       ff=0;
     }
   }
